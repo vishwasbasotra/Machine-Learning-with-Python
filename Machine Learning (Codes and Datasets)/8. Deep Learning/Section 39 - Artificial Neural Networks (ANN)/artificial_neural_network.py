e6681d60af241b30adc25562eb882195ae2d79c4
brigu97 <brigu.sharma.9@gmail.com>
2020-05-20 23:24:35 +0530

    Artificial Nueral Networks (Stochastic Gradient Descent)

--- Machine Learning (Codes and Datasets)/8. Deep Learning/Section 39 - Artificial Neural Networks (ANN)/artificial_neural_network.py
+++ Machine Learning (Codes and Datasets)/8. Deep Learning/Section 39 - Artificial Neural Networks (ANN)/artificial_neural_network.py
@@ -28,15 +28,17 @@ ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remaind
 X = np.array(ct.fit_transform(X))
 print(X)
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X = sc.fit_transform(X)
-print(X)
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train =sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+print(X_train,X_test)
 
 # Part 2 - Building the ANN
 
